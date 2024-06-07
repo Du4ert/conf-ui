8f2f5d66f40b0cd18133843381342cb1199e28b3
Du4ert <du4ert@gmail.com>
2024-06-07 19:38:12 +0300

    layout

--- resources/views/sections/en/en_committee.blade.php
+++ resources/views/sections/en/en_committee.blade.php
@@ -2,34 +2,37 @@
     <div class="container">
 
 
-        <div class="col-6 col-lg-3 mb-4">
-            <div class="card rounded-0">
-                <img src="{{asset('/images/orgs/org-4.jpg')}}" class="card-img-top rounded-0" alt="">
-                <div class="card-body">
-                    <h5 class="card-title mb-2">Ю. В. Плугатарь</h5>
-                    <div class="card-text mb-3">
-                        <div class="meta">чл.-корр. РАН, д.с.-х.н., директор ФГБУН «НБС-ННЦ»</div>
-                        <strong class="meta">Сопредседатель</strong>
-                    </div><!--//card-text-->
-                </div><!--//card-->
-            </div><!--//card-->
-        </div><!--//col-->
-        <div class="col-6 col-lg-3 mb-4">
-            <div class="card rounded-0">
-                <img src="{{asset('/images/orgs/org-1.jpg')}}" class="card-img-top rounded-0" alt="">
-                <div class="card-body">
-                    <h5 class="card-title mb-2">Е. А. Водясова</h5>
-                    <div class="card-text mb-3">
-                        <div class="meta">к. б. н., руководитель Курчатовского геномного центра – НБС-ННЦ</div>
-                        <strong class="meta">Сопредседатель</strong>
-                    </div><!--//card-text-->
-                </div><!--//card-->
-            </div><!--//card-->
-        </div><!--//col-->
-
         <h3 class="section-heading text-center mb-3">Committee</h3>
         <div class="section-intro text-center single-col-max mx-auto mb-5">Introductory text</div>
         <div class="row">
+
+
+            <div class="col-6 col-lg-3 mb-4">
+                <div class="card rounded-0">
+                    <img src="{{asset('/images/orgs/org-4.jpg')}}" class="card-img-top rounded-0" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title mb-2">Ю. В. Плугатарь</h5>
+                        <div class="card-text mb-3">
+                            <div class="meta">чл.-корр. РАН, д.с.-х.н., директор ФГБУН «НБС-ННЦ»</div>
+                            <strong class="meta">Сопредседатель</strong>
+                        </div><!--//card-text-->
+                    </div><!--//card-->
+                </div><!--//card-->
+            </div><!--//col-->
+            <div class="col-6 col-lg-3 mb-4">
+                <div class="card rounded-0">
+                    <img src="{{asset('/images/orgs/org-1.jpg')}}" class="card-img-top rounded-0" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title mb-2">Е. А. Водясова</h5>
+                        <div class="card-text mb-3">
+                            <div class="meta">к. б. н., руководитель Курчатовского геномного центра – НБС-ННЦ</div>
+                            <strong class="meta">Сопредседатель</strong>
+                        </div><!--//card-text-->
+                    </div><!--//card-->
+                </div><!--//card-->
+            </div><!--//col-->
+
+
             <div class="col-6 col-lg-3 mb-4">
                 <div class="card rounded-0">
                     <img src="{{asset('/images/orgs/org-3.jpg')}}" class="card-img-top rounded-0" alt="">
