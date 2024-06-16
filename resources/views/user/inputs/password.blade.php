cf6f75233c2869095d28e72d1db385e74ab2b2ca
Du4ert <du4ert@gmail.com>
2024-06-16 15:44:22 +0300

    confirm password *

--- resources/views/user/inputs/password.blade.php
+++ resources/views/user/inputs/password.blade.php
@@ -22,7 +22,7 @@ if (!isset($form)) {
 </div>
 
 <div class="row mb-3">
-    <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('auth.password_confirm') }}</label>
+    <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('auth.password_confirm') }}<span class="text-danger">*</span></label>
 
     <div class="col-md-6">
         <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
