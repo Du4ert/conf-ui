c675bb9d856fe79ea8fcb09be63291d2b63024b4
Du4ert <du4ert@gmail.com>
2024-07-12 09:56:15 +0300

    sponsors change

--- resources/views/sections/en/sponsors.blade.php
+++ resources/views/sections/en/sponsors.blade.php
@@ -3,16 +3,17 @@
         <h3 class="section-heading text-center mb-3">Sponsors and partners</h3>
         {{-- <div class="section-intro text-center single-col-max mx-auto mb-5">Want to become a sponsor? Get in touch lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat magna eu accumsan mattis.</div> --}}
         <div class="row logos justify-content-center  align-items-center">
-            <div class="logo-item col-6 col-md-4 col-lg-2"><img class="w-75" src="{{asset('/images/sponsors/sponsor-12.png')}}" alt="Nikita Botanical Garden" title="Nikita Botanical Garden"></div>
-            {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-1.png')}}" alt="Orel"></div> --}}
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://obr.nbgnsc.ru/" target="_blank"><img class="w-75" src="{{asset('/images/sponsors/sponsor-12.png')}}" alt="Nikita Botanical Garden" title="Nikita Botanical Garden"></a></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://minobrnauki.gov.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-1.png')}}" alt="Ministry of Science and Higher Education of the Russian Federation" title="Ministry of Science and Higher Education of the Russian Federation"></a></div>
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img class="w-85" src="{{asset('/images/sponsors/sponsor-7.png')}}" alt="AlaMed"></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img class="w-85" src="{{asset('/images/sponsors/sponsor-10.png')}}" alt="Spektronika"></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-4.png')}}" alt="National Research Center "Kurchatov Institute""></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img class="w-90" src="{{asset('/images/sponsors/sponsor-6.png')}}" alt="ИЦГ"></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-3.png')}}" alt="Euro"></div> --}}
-            <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-8.png')}}" alt="Kurchatov Genome Center" title="Kurchatov Genome Center"></div>
-            <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-9.png')}}" alt="Syntol" title="Syntol"></div>
-            <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-2_en.png')}}" alt="the Russian Academy of Sciences" title="the Russian Academy of Sciences"></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://genrussia.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-8.png')}}" alt="Kurchatov Genome Center" title="Kurchatov Genome Center"></a></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://www.syntol.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-9.png')}}" alt="Syntol" title="Syntol"></a></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://www.dia-m.ru/" target="_blank"><img src="{{asset('/images/sponsors/diaem_en.svg')}}" alt="dia-m.ru" title="dia-m.ru"></a></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://www.ras.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-2_en.png')}}" alt="the Russian Academy of Sciences" title="the Russian Academy of Sciences"></a></div>
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-5.png')}}" alt="Russian Scientific Fund"></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-11.png')}}" alt="Sevastopol television"></div> --}}
         </div><!--//row-->
--- resources/views/sections/ru/sponsors.blade.php
+++ resources/views/sections/ru/sponsors.blade.php
@@ -3,16 +3,17 @@
         <h3 class="section-heading text-center mb-3">Спонсоры и Партнёры</h3>
         {{-- <div class="section-intro text-center single-col-max mx-auto mb-5">Want to become a sponsor? Get in touch lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat magna eu accumsan mattis.</div> --}}
         <div class="row logos justify-content-center  align-items-center">
-            <div class="logo-item col-6 col-md-4 col-lg-2"><img class="w-75" src="{{asset('/images/sponsors/sponsor-12.png')}}" title="Никитский ботанический сад" alt="Никитский ботанический сад"></div>
-            {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-1.png')}}" alt="Орёл"></div> --}}
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://obr.nbgnsc.ru/" target="_blank"><img class="w-75" src="{{asset('/images/sponsors/sponsor-12.png')}}" title="Никитский ботанический сад" alt="Никитский ботанический сад"></a></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://minobrnauki.gov.ru/"><img src="{{asset('/images/sponsors/sponsor-1.png')}}" alt="Министерство науки и высшего образования РФ" title="Министерство науки и высшего образования РФ"></a></div>
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img class="w-85" src="{{asset('/images/sponsors/sponsor-7.png')}}" alt="AlaMed"></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img class="w-85" src="{{asset('/images/sponsors/sponsor-10.png')}}" alt="Spektronika"></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-4.png')}}" alt="Национальный исследовательский центр «Курчатовский институт»"></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img class="w-90" src="{{asset('/images/sponsors/sponsor-6.png')}}" alt="ИЦГ"></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-3.png')}}" alt="Евроген"></div> --}}
-            <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-8.png')}}" alt="Курчатовский геномный центр" title="Курчатовский геномный центр"></div>
-            <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-9.png')}}" alt="Синтол" title="Синтол"></div>
-            <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-2_ru.png')}}" alt="Российская академия наук" title="Российская академия наук"></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://genrussia.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-8.png')}}" alt="Курчатовский геномный центр" title="Курчатовский геномный центр"></a></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://www.syntol.ru/" taget="_blank"><img src="{{asset('/images/sponsors/sponsor-9.png')}}" alt="Научно производственная компания Синтол" title="Синтол"></a></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://www.dia-m.ru/" target="_blank"><img src="{{asset('/images/sponsors/diaem.jpg')}}" alt="Диаэм" title="Диаэм"></a></div>
+            <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://www.ras.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-2_ru.png')}}" alt="Российская академия наук" title="Российская академия наук"></a></div>
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-5.png')}}" alt="Российский научный фонд"></div> --}}
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><img src="{{asset('/images/sponsors/sponsor-11.png')}}" alt="Севастопольское телевидение"></div> --}}
         </div><!--//row-->
