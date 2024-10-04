26bb761dae25d53e1559aa0e19940044f52d4d76
Du4ert <du4ert@gmail.com>
2024-10-04 15:39:10 +0300

    add vniisib to sponsors

--- resources/views/sections/ru/sponsors.blade.php
+++ resources/views/sections/ru/sponsors.blade.php
@@ -3,14 +3,15 @@
         <h3 class="section-heading text-center mb-3">Спонсоры и Партнёры</h3>
         {{-- <div class="section-intro text-center single-col-max mx-auto mb-5">Want to become a sponsor? Get in touch lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat magna eu accumsan mattis.</div> --}}
         <div class="row logos justify-content-center  align-items-center">
-            <div class="offset-lg-1 offset-0"></div>
+            {{-- <div class="offset-lg-1 offset-0"></div> --}}
             <div class="logo-item col-6 col-md-3 col-lg-2"><a href="https://obr.nbgnsc.ru/" target="_blank"><img class="w-75" src="{{asset('/images/sponsors/sponsor-12.png')}}" title="Никитский ботанический сад" alt="Никитский ботанический сад"></a></div>
+            <div class="logo-item col-6 col-md-3 col-lg-2"><a href="https://vniisb.ru/" target="_blank"><img class="w-75" src="{{asset('/images/sponsors/vniisib.png')}}" title="ФГБНУ «Всероссийский научно-исследовательский институт сельскохозяйственной биотехнологии» (ФГБНУ ВНИИСБ)" alt="ФГБНУ «Всероссийский научно-исследовательский институт сельскохозяйственной биотехнологии» (ФГБНУ ВНИИСБ)"></a></div>
             <div class="logo-item col-6 col-md-3 col-lg-2"><a href="https://vogis.org" target="_blank"><img src="{{asset('/images/sponsors/vogis.png')}}" alt="Межрегиональная общественная организация Вавиловское общество генетиков и селекционеров" title="Межрегиональная общественная организация Вавиловское общество генетиков и селекционеров"></a></div>
             <div class="logo-item col-6 col-md-3 col-lg-2"><a href="https://minobrnauki.gov.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-1.png')}}" alt="Министерство науки и высшего образования РФ" title="Министерство науки и высшего образования РФ"></a></div>
             {{-- <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://genrussia.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-8.png')}}" alt="Курчатовский геномный центр" title="Курчатовский геномный центр"></a></div> --}}
             <div class="logo-item col-6 col-md-3 col-lg-2"><a href="https://www.dia-m.ru/" target="_blank"><img src="{{asset('/images/sponsors/diaem.jpg')}}" alt="ООО «ДИАЭМ»/ DIA-M" title="ООО «ДИАЭМ»/ DIA-M"></a></div>
             <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://www.ras.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-2_ru.png')}}" alt="Российская академия наук" title="Российская академия наук"></a></div>
-            <div class="offset-lg-1 offset-0"></div>
+            {{-- <div class="offset-lg-1 offset-0"></div> --}}
             <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://www.syntol.ru/" target="_blank"><img src="{{asset('/images/sponsors/sponsor-9.png')}}" alt="ООО «НПФ СИНТОЛ»/ SINTOL" title="Синтол"></a></div>
             <div class="logo-item col-6 col-md-4 col-lg-2"><a href="https://belbiolab.ru" target="_blank"><img src="{{asset('/images/sponsors/beliolab.jpg')}}" alt="ООО «БелБиоЛаб»/ BelBioLab" title="ООО «БелБиоЛаб»/ BelBioLab"></a></div>
             <div class="logo-item col-6 col-md-3 col-lg-2"><a href="https://biolabmix.ru" target="_blank"><img src="{{asset('/images/sponsors/biolabmix.png')}}" alt="ООО «БИОЛАБМИКС»/ BIOLABMIX" title="ООО «БИОЛАБМИКС»/ BIOLABMIX"></a></div>
